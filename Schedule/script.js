3ee8fec747ff71863c060d8c713eda94d683bb3a
Shane Bogue <111470027+Shane-Bogue@users.noreply.github.com>
2023-03-31 18:24:48 -0700

    Add files via upload

--- Schedule/script.js
+++ Schedule/script.js
@@ -142,3 +142,19 @@ class Display {
 
 Calendar.Generate(Calendar.year, Calendar.month)
 UI.Initialize()
+
+const modal = document.getElementById("myModal");
+const btns = document.querySelectorAll("#modalBtn");
+const  span = document.getElementsByClassName("close")[0];
+
+btns.forEach(btn => btn.addEventListener('click', () => modal.style.display = "block"))
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
